085d6b849f11506fe19652976b828bad65943e8d
Edi Ubaedillah <ubaedillahedi@gmail.com>
2023-05-29 08:20:18 +0700

    create log console

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const expressWinston = require("express-winston");
+const { transports, format } = require("winston");
+
+app.use(
+  expressWinston.logger({
+    transports: [new transports.Console()],
+    format: format.json(),
+  })
+);
 
 app.get("/", (req, res) => {
   res.sendStatus(200);
 });
-
 
 app.listen(5001);
